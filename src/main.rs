90bac461554cb5a9ecae07b051ee4071dc895db0
Digital Quinn <quinyonnave77@gmail.com>
2023-02-11 13:48:54 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use std::process::Command;
+use std::str;
 
 fn main() {
-    let ip_address = "1.1.1.1";
-    let output = Command::new("ping")
-        .arg(ip_address)
+    let output = Command::new("reg")
+        .args(&["query", "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\", "/s"])
         .output()
-        .expect("Failed to run command");
+        .expect("Failed to execute 'reg' command");
+    let registry = str::from_utf8(&output.stdout).unwrap();
 
-    let output_str = String::from_utf8_lossy(&output.stdout);
-    println!("{}", output_str);
+    for line in registry.lines() {
+        if line.contains("    (Default)    REG_SZ    ") {
+            let key = line.split("    ").nth(0).unwrap();
+            let value = line.split("    ").nth(3).unwrap();
+            if value.contains(" ") {
+                println!("{}", key);
+            }
+        }
+    }
 }
