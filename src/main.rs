c16115f5d06bb7bf735ed855321d0f0d404f9ca0
Digital Quinn <quinyonnave77@gmail.com>
2023-02-11 12:35:44 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use std::fs;
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("cmd")
-        .args(&["/C", "tpmtool.exe", "drivertracing", "stop"])
-        .output()
-        .expect("failed to execute process");
+    let contents = fs::read_to_string("cmd.txt")
+        .expect("Something went wrong reading the file");
 
-    let result = String::from_utf8_lossy(&output.stdout);
-    println!("{}", result);
+    let commands = contents.lines();
+
+    for cmd in commands {
+        let output = Command::new("cmd")
+            .args(&["/C", cmd])
+            .output()
+            .expect("failed to execute process");
+
+        let result = String::from_utf8_lossy(&output.stderr);
+        if !result.contains("is not recognized") {
+            println!("{}", result);
+        }
+    }
 }
