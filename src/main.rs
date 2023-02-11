b651ce417e65aa48227c032199e03dc8127968a2
Digital Quinn <quinyonnave77@gmail.com>
2023-02-11 13:26:18 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,54 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
-use std::process::{Command, Stdio};
+use std::process::Command;
+use std::io::{self, Read};
+
+fn generate_example_command(command: &str) -> String {
+    // Parse the command string to determine the command name and its parameters
+    let parts: Vec<&str> = command.split_whitespace().collect();
+    let command_name = parts[0];
+    let parameters = parts[1..].join(" ");
+
+    // Generate an example command string based on the command name and parameters
+    let example_command = match command_name {
+        "dir" => format!("dir {}", parameters),
+        "echo" => format!("echo {}", parameters),
+        // Add cases for other commands as needed
+        _ => format!("{} {}", command_name, parameters),
+    };
+
+    example_command
+}
 
 fn main() {
-    let cmd_file_path = "C:\\Users\\Qu1nSpl0it\\Desktop\\rust\\Windows-Commands\\cmd.txt";
-    let cmd_file = match File::open(cmd_file_path) {
+    // Read the commands from the file
+    let mut file = match std::fs::File::open("C:\\Users\\Qu1nSp0it\\Desktop\\rust\\Windows-Commands\\cmd.txt") {
         Ok(file) => file,
-        Err(e) => {
-            println!("Error opening file: {}", e);
+        Err(error) => {
+            println!("Error opening file: {}", error);
             return;
         },
     };
-    let cmd_reader = BufReader::new(cmd_file);
-    for line in cmd_reader.lines() {
-        let cmd = line.unwrap();
-        let quoted_cmd = format!("\"{}\"", cmd);
+
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Ok(_) => {},
+        Err(error) => {
+            println!("Error reading file: {}", error);
+            return;
+        },
+    };
+
+    // Iterate over each line in the file and run the command
+    for line in contents.lines() {
+        let example_command = generate_example_command(line);
         let output = Command::new("cmd")
-            .arg("/C")
-            .arg(&quoted_cmd)
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .output();
-        match output {
-            Ok(o) => {
-                if !o.status.success() {
-                    let stderr = String::from_utf8_lossy(&o.stderr);
-                    if stderr.contains("is not recognized as an internal or external command") {
-                        println!("Command not recognized: {:?}", cmd);
-                    } else {
-                        let parts: Vec<&str> = cmd.split(" ").collect();
-                        let mut example_cmd = String::new();
-                        example_cmd.push_str(parts[0]);
-                        example_cmd.push_str(" ");
-                        for i in 1..parts.len() {
-                            example_cmd.push_str("<");
-                            example_cmd.push_str(parts[i]);
-                            example_cmd.push_str(">");
-                            example_cmd.push_str(" ");
-                        }
-                        println!("Error: {:?}\nExample command: {:?}", stderr, example_cmd);
-                    }
-                } else {
-                    let stdout = String::from_utf8_lossy(&o.stdout);
-                    let mut output_file = File::create("output.txt").unwrap();
-                    output_file.write_all(stdout.as_bytes()).unwrap();
-                }
-            },
-            Err(e) => println!("Failed to run command: {:?}", e),
+            .args(&["/C", &example_command])
+            .output()
+            .expect("Failed to run command");
+
+        if !output.status.success() {
+            println!("Error running command: {}", String::from_utf8_lossy(&output.stderr));
+        } else {
+            println!("{}", String::from_utf8_lossy(&output.stdout));
         }
     }
 }
