68b9c350d07866b52a8c50b5144d23833be8b0eb
Digital Quinn <quinyonnave77@gmail.com>
2023-02-11 11:35:01 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,7 @@ fn main() {
         .expect("failed to execute process");
 
     let commands = String::from_utf8_lossy(&output.stdout);
-    let commands: Vec<&str> = commands.split('\n').collect();
-
-    for command in commands.iter() {
-        if command.starts_with("C:\\Windows\\System32") {
-            let split = command.split("\\");
-            let name = split.last().unwrap();
-            println!("{}", name);
-        }
+    for command in commands.lines() {
+        println!("{}", command);
     }
 }
