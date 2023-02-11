c95b765a24331bed98a0686e02ebfd642726c29c
Digital Quinn <quinyonnave77@gmail.com>
2023-02-11 14:34:27 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@ use std::process::Command;
 use std::str;
 
 fn main() {
-    let output = Command::new("reg")
-        .args(&["query", "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\", "/s"])
-        .output()
-        .expect("Failed to execute 'reg' command");
-    let registry = str::from_utf8(&output.stdout).unwrap();
+    let hives = ["HKLM", "HKCC", "HKCR", "HKU", "HKCU"];
+    for hive in hives.iter() {
+        let output = Command::new("reg")
+            .args(&["query", format!("{}\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\", hive), "/s"])
+            .output()
+            .expect("Failed to execute 'reg' command");
+        let registry = str::from_utf8(&output.stdout).unwrap();
 
-    for line in registry.lines() {
-        if line.contains("    (Default)    REG_SZ    ") {
-            let key = line.split("    ").nth(0).unwrap();
-            let value = line.split("    ").nth(3).unwrap();
-            if value.contains(" ") {
-                println!("{}: {}", key, value);
+        for line in registry.lines() {
+            if line.contains("    (Default)    REG_SZ    ") {
+                let key = line.split("    ").nth(0).unwrap();
+                let value = line.split("    ").nth(3).unwrap();
+                if value.contains(" ") {
+                    println!("{}: {}", key, value);
+                }
             }
         }
     }
