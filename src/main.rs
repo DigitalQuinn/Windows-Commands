d316f02e2b8dd33f25cf518987e9c651d752e234
Digital Quinn <quinyonnave77@gmail.com>
2023-02-11 12:43:39 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-use std::fs;
-use std::process::Command;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
+use std::env;
 
 fn main() {
-    let contents = fs::read_to_string("cmd.txt")
-        .expect("Something went wrong reading the file");
+    let current_dir = env::current_dir().unwrap();
+    println!("Current working directory: {:?}", current_dir);
 
-    let commands = contents.lines();
-
-    for cmd in commands {
-        let output = Command::new("cmd")
-            .args(&["/C", cmd])
-            .output()
-            .expect("failed to execute process");
-
-        let result = String::from_utf8_lossy(&output.stderr);
-        if !result.contains("is not recognized") {
-            println!("{}", result);
+    let cmd_file = File::open("cmd.txt").unwrap();
+    let cmd_reader = BufReader::new(cmd_file);
+    for line in cmd_reader.lines() {
+        let cmd = line.unwrap();
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg(cmd)
+            .current_dir(&current_dir)
+            .stdout(Stdio::null())
+            .stderr(Stdio::piped())
+            .output();
+        match output {
+            Ok(o) => {
+                if !o.status.success() {
+                    let stderr = String::from_utf8_lossy(&o.stderr);
+                    if stderr.contains("command not found") {
+                        println!("Command not recognized: {:?}", cmd);
+                    } else {
+                        println!("{:?}", stderr);
+                    }
+                }
+            },
+            Err(e) => println!("Failed to run command: {:?}", e),
         }
     }
 }
