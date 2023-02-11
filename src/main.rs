b1a2c22a039aa74e6149675696c53a603c32157f
Digital Quinn <quinyonnave77@gmail.com>
2023-02-11 13:44:42 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
-use std::fs;
 use std::process::Command;
 
 fn main() {
-    let cmd_file = "C:\\Users\\Qu1nSpl0it\\Desktop\\rust\\Windows-Commands\\cmd.txt";
-    let output_file = "output.txt";
-    let cmds = fs::read_to_string(cmd_file)
-        .expect("Failed to read file");
+    let ip_address = "1.1.1.1";
+    let output = Command::new("ping")
+        .arg(ip_address)
+        .output()
+        .expect("Failed to run command");
 
-    for cmd in cmds.lines() {
-        let output = Command::new("cmd")
-            .args(&["/C", cmd])
-            .output()
-            .expect("Failed to run command");
-        
-        let output_str = String::from_utf8_lossy(&output.stdout).to_string();
-        let error_str = String::from_utf8_lossy(&output.stderr).to_string();
-
-        if !error_str.is_empty() {
-            println!("Error: {}", error_str);
-        } else {
-            fs::write(output_file, output_str)
-                .expect("Failed to write file");
-        }
-    }
+    let output_str = String::from_utf8_lossy(&output.stdout);
+    println!("{}", output_str);
 }
